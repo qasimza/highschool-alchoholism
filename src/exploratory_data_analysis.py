67bc0906bc9d2240af1c8bae066a33b73546f3b1
qasimza <qasimza@mail.uc.edu>
2024-02-01 23:07:18 -0500

    Pushing most recent updates

--- src/exploratory_data_analysis.py
+++ src/exploratory_data_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 FILE_PATH = './data/EN_Dataset/en_lpor_explorer.csv'
 
@@ -20,15 +21,20 @@ print(df.describe(), '\n')
 for col in df.columns:
  print(df[col].value_counts(), '\n')
 
+# Preliminary visualization
+sns.pairplot(df, x_vars={'Alcohol_Weekdays', 'Alcohol_Weekends', 'Grade_1st_Semester', 'Grade_2nd_Semester'})
+plt.show()
+
 """
 Data Cleaning and Transformation 
 """
 
-# Exclude data with missing values
-df = df.dropna()
-
-# Applying ___ transformation to data
-df['severity'] = df['severity'].map({'Low': 1, 'Medium': 2, 'High': 3, 'Very High': 4})
+# Applying ordinal encoding transformation to data
+parent_education_mapping = {'na': 0, 'Primary School': 1, 'Lower Secondary School': 2, 'High School': 3,
+                            'Higher Education': 4}
+df['Mother_Education'] = df['Mother_Education'].fillna(0).map(parent_education_mapping)
+df['Father_Education'] = df['Father_Education'].fillna(0).map(parent_education_mapping)
+df['Father_Education'] = df['Father_Education'].map(parent_education_mapping)
 
 
 """
